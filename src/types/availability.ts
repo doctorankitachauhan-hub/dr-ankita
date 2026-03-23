55d92d54b9a2f3afb6f32ffdc34d60e992df75eb
priyesshrai <priyeshrai369@gmail.com>
2026-03-23 15:04:14 +0530

    update schema

--- src/types/availability.ts
+++ src/types/availability.ts
@@ -1,12 +1,12 @@
-import z from "zod";
+import { z } from "zod";
 
 export const AvailabilitySchema = z.object({
-    availability: z.array(
-        z.object({
-            dayOfWeek: z.number().min(0).max(7),
-            startTime: z.iso.datetime({ error: "Start time is required" }),
-            endTime: z.iso.datetime({ error: "End time is required" }),
-            slotDuration: z.number().min(5)
-        })
-    )
+  availability: z.array(
+    z.object({
+      dayOfWeek: z.number().min(0).max(6),
+      startTime: z.string(),
+      endTime: z.string(),
+      slotDuration: z.number().min(5)
+    })
+  )
 });
