97fca02aee1a32f0d5b083d77d1bbaf2dbd1ca06
priyesshrai <priyeshrai369@gmail.com>
2026-03-23 12:50:21 +0530

    update dashboar page for doctor

--- src/app/(dashboard)/(doctor)/doctor/layout.tsx
+++ src/app/(dashboard)/(doctor)/doctor/layout.tsx
@@ -1,10 +1,16 @@
+import Sidebar from '@/components/common/sidebar/sidebar';
+import DashboardTopbar from '@/components/common/topbar/topbar';
 import React, { ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast';
 
 export default function DoctorLayout({ children }: Readonly<{ children: ReactNode; }>) {
     return (
         <main className="flex min-h-screen">
-            {children}
+            <Sidebar />
+            <section className='p-0! flex-1 w-full min-h-screen bg-gray-50 flex flex-col'>
+                <DashboardTopbar />
+                {children}
+            </section>
             <Toaster />
         </main>
     )
